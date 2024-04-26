e7aefca1d9b4dc78a3443552ddb6c29b84ba1af2
eycy <96989651+eycy@users.noreply.github.com>
2024-04-26 06:39:41 +0000

    feat: implement delete button

--- src/components/Dogs.tsx
+++ src/components/Dogs.tsx
@@ -10,6 +10,24 @@ import axios from "axios";
 
 const Dogs = ({ credentials, isLoggedIn, dogs, setDogs }) => {
 
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${dogAPI.url}/api/v1/dogs/${id}`, {
+        headers: {
+          'Authorization': `Basic ${credentials}`
+        }
+      });
+      // Remove the deleted dog from the dogs array
+      const updatedDogs = dogs.filter((dog) => dog.id !== id);
+      setDogs(updatedDogs);
+    } catch (error) {
+      // Handle the error
+      console.error(error);
+    }
+  };
+
+
   React.useEffect(() => {
     axios.get(`${dogAPI.url}/api/v1/dogs`, {
       headers: {
@@ -28,14 +46,20 @@ const Dogs = ({ credentials, isLoggedIn, dogs, setDogs }) => {
 
     return (
       <>
-        
-        
+
+
         <Flex justify="space-evenly" wrap="wrap" gap="middle">
           {
             dogs &&
             dogs.map(({ id, name, description }) => (
               <>
-                <Dog isLoggedIn={isLoggedIn} name={name} key={id} href={id}>{description}</Dog>
+                <Dog
+                  isLoggedIn={isLoggedIn}
+                  name={name}
+                  key={id}
+                  href={id}
+                  handleDelete={() => handleDelete(id)}
+                />
               </>
             ))
           }
