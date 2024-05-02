a6a01ef95365b886961f438ffb99ee08888baeb1
eycy <96989651+eycy@users.noreply.github.com>
2024-05-02 13:06:29 +0000

    fix: update listing form alignment

--- src/components/ListingForm.tsx
+++ src/components/ListingForm.tsx
@@ -54,13 +54,13 @@ const ListingForm = ({ credentials, isLoggedIn, isEditMode }) => {
     // Convert authorID to integer
     const authorID = parseInt(values.authorID, 10);
 
-    let method = 'POST' 
+    let method = 'POST'
     let url = `${dogAPI.url}/api/v1/dogs`
     if (isEditMode) {
       method = 'PUT';
       url = `${dogAPI.url}/api/v1/dogs/${selectedDog.id}`
     }
-      
+
     const response = await fetch(url, {
       method: method,
       headers: {
@@ -99,59 +99,62 @@ const ListingForm = ({ credentials, isLoggedIn, isEditMode }) => {
   return (
     <>
       {isLoggedIn ? (
-        <div style={{ textAlign: 'left', marginLeft: '20px' }}>
-          {isEditMode ? (<Title>Edit Listing</Title>) : (<Title>Create New Listing</Title>)}
-          <Form
-            labelCol={{ span: 4 }}
-            labelAlign="right"
-            wrapperCol={{ span: 14 }}
-            layout="horizontal"
-            initialValues={initialValues}
-            onValuesChange={onFormLayoutChange}
-            size={componentSize as SizeType}
-            style={{ maxWidth: 600, marginTop: '20px' }}
-            form={form}
-            onFinish={handleSubmit}
-          >
-            <Form.Item label="Name" name="name">
-              <Input />
-            </Form.Item>
-            <Form.Item label="Breed" name="breed_id">
-              <Select>
-                <Select.Option value={1}>Akita</Select.Option>
-                <Select.Option value={2}>Bouvier</Select.Option>
-                <Select.Option value={3}>Dachshund</Select.Option>
-                <Select.Option value={4}>German Shepherd</Select.Option>
-                <Select.Option value={5}>Otterhound</Select.Option>
-                <Select.Option value={6}>Terrier</Select.Option>
-              </Select>
-            </Form.Item>
-            <Form.Item label="Location" name="location">
-              <Select>
-                <Select.Option value="Aberdeen">Aberdeen</Select.Option>
-                <Select.Option value="Jordan">Jordan</Select.Option>
-                <Select.Option value="Shatin">Shatin</Select.Option>
-              </Select>
-            </Form.Item>
-            <Form.Item label="Date of Birth" name="dob">
-              <DatePicker />
-            </Form.Item>
-            <Form.Item label="Description" name="description">
-              <TextArea rows={4} />
-            </Form.Item>
-            <Form.Item label="Author ID" name="authorID">
-              <Input />
-            </Form.Item>
-            <div style={{ marginLeft: '20px' }}>
-              <Space>
-                <Button type="primary" htmlType="submit">Submit</Button>
-                <Button htmlType="button" onClick={onReset}>
-                  Reset
-                </Button>
-                <Button htmlType="button" onClick={() => navigate(-1)}>Cancel</Button>
-              </Space>
-            </div>
-          </Form>
+
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div style={{ width: '50%' }}>
+            {isEditMode ? (<Title>Edit Listing</Title>) : (<Title>Create New Listing</Title>)}
+            <Form
+              labelCol={{ span: 4 }}
+              labelAlign="right"
+              wrapperCol={{ span: 14 }}
+              layout="horizontal"
+              initialValues={initialValues}
+              onValuesChange={onFormLayoutChange}
+              size={componentSize as SizeType}
+              style={{ maxWidth: 600, marginTop: '20px' }}
+              form={form}
+              onFinish={handleSubmit}
+            >
+              <Form.Item label="Name" name="name">
+                <Input />
+              </Form.Item>
+              <Form.Item label="Breed" name="breed_id" style={{ textAlign: 'left' }}>
+                <Select>
+                  <Select.Option value={1}>Akita</Select.Option>
+                  <Select.Option value={2}>Bouvier</Select.Option>
+                  <Select.Option value={3}>Dachshund</Select.Option>
+                  <Select.Option value={4}>German Shepherd</Select.Option>
+                  <Select.Option value={5}>Otterhound</Select.Option>
+                  <Select.Option value={6}>Terrier</Select.Option>
+                </Select>
+              </Form.Item>
+              <Form.Item label="Location" name="location" >
+                <Select >
+                  <Select.Option value="Aberdeen">Aberdeen</Select.Option>
+                  <Select.Option value="Jordan">Jordan</Select.Option>
+                  <Select.Option value="Shatin">Shatin</Select.Option>
+                </Select>
+              </Form.Item>
+              <Form.Item label="Date of Birth" name="dob" style={{ textAlign: 'left' }} >
+                <DatePicker />
+              </Form.Item>
+              <Form.Item label="Description" name="description">
+                <TextArea rows={4} />
+              </Form.Item>
+              <Form.Item label="Author ID" name="authorID">
+                <Input />
+              </Form.Item>
+              <div style={{ marginLeft: '20px' }}>
+                <Space>
+                  <Button type="primary" htmlType="submit">Submit</Button>
+                  <Button htmlType="button" onClick={onReset}>
+                    Reset
+                  </Button>
+                  <Button htmlType="button" onClick={() => navigate(-1)}>Cancel</Button>
+                </Space>
+              </div>
+            </Form>
+          </div>
         </div>
       ) : <div>Access denied. Please login.</div>}
     </>
